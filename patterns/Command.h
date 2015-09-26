3ea0cecec265b4f2778ad9df872830270bcf9424
Sergey Vaycel <seralwei@gmail.com>
2015-09-26 12:08:29 +0600

    Implement MethodExecuteCommand, start write ThreadManager description (like a 'how i must do')

--- patterns/Command.h
+++ patterns/Command.h
@@ -1,10 +1,35 @@
-namespace patterns {
+#include <memory>
 
-  template<typename Arg0, typename... Args>
+namespace patterns {
+  using std::shared_ptr;
+
+  template<typename... Args>
   class Command {
+  protected:
+    virtual Command ( ) = 0;
+    virtual ~Command ( ) = 0;
   public:
     //If you will know args count : const unsigned numargs = sizeof...(Args);
-    virtual void execute (const Arg0&, const Args& ...) = 0;
+    virtual void execute (const Args& ...) = 0;
+  };
+
+  template<class Subject, typename... Args>
+  class MethodExecuteCommand : public Command<typename... Args> {
+  private:
+    void (Subject:: *Action) (Args...);
+
+    shared_ptr<Subject> subject_;
+    shared_ptr<Action> action_;
+  public:
+    MethodExecuteCommand (shared_ptr<Subject> s, shared_ptr<Action> a) 
+      : subject_(s), action_(a) {};
+    ~MethodExecuteCommand ( ) {
+      delete subject_;
+      delete action_;
+    };
+    void exectute (const Args& aa...) {
+      this->subject_ ->* this->action_ (aa...);
+    };
   };
 
 }
