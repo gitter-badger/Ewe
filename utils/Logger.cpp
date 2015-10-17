b2777f9a5ebc76dc944df725736011b4423c6243
Sergey Vaytsel <seralwei@gmail.com>
2015-10-17 10:47:42 +0600

    logger. wip commit

--- utils/Logger.cpp
+++ utils/Logger.cpp
@@ -5,3 +5,24 @@ Logger::Logger ( ) { }
 
 
 Logger::~Logger ( ) { }
+
+
+void Logger::debug(std::string message) {
+
+}
+
+void Logger::info(std::string message)  {
+
+}
+
+void Logger::warn(std::string message)  {
+
+}
+
+void Logger::error(std::string message) {
+
+}
+
+void Logger::fatal(std::string message) {
+
+}
--- utils/Logger.h
+++ utils/Logger.h
@@ -1,10 +1,18 @@
 #ifndef LOGGER_H_
 #define LOGGER_H_
 
+#include <string>
+
 class Logger {
 public:
   Logger ( );
   ~Logger ( );
+
+  void debug(std::string message);
+  void info(std::string message);
+  void warn(std::string message);
+  void error(std::string message);
+  void fatal(std::string message);
 };
 
 #endif // LOGGER_H_
